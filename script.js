5d8fd42831a5728ce61ac3f44f2afa9791c38a11
Mauricio Dimate <dima0042@algonquinlive.com>
2020-10-11 09:13:06 -0400

    final

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
-const boom = document.querySelector("#boom"); 
-boom.innerText = String.fromCodePoint(0x1F52A,0x1F4A3,0x1F601,0x1f603,0x1f607,0x1f605);
+let $container = document.getElementById('container')
+let container = []
 
 
-const emojis = document.querySelector("#emojis");
-emojis.innerText = String.fromCodePoint(0x1F4A1,0x1F4A2,0x1F4A4,0x1F4A5,0x1F4A6,0x1F4A7,0x1F4A8);
+for (let i = 127744; i <= 127776; i++) {
+    container.push(`<div class="emoji">&#${i}</div><div class="code">${i}</div>`)
+    };
+for (let i =127791; i <= 127796; i++){
+        container.push(`<div class="emoji">&#${i}</div><div class="code">${i}</div>`)
+        };
 
-const emojix = document.querySelector("#emojix");
-emojix.innerText = String.fromCodePoint(0x1F6A3,0x1F6A1,0x1F6A2,0x1F6A4,0x1F6A0,0x1F6A7,0x1F6A8);
+ for (let i =9800; i <= 9810; i++){
+            container.push(`<div class="emoji">&#${i}</div><div class="code">${i}</div>`)
+            }
 
-const halloween = document.querySelector("#halloween");
-halloween.innerText = String.fromCodePoint(0x1F611,0x1F435,0x1F436,0x1F437,0x1F438,0x1F439,0x1F434);
-
-const smiles = document.querySelector("#smiles");
-smiles.innerText = String.fromCodePoint(0x1F354,0x1F372,0x1F373,0x1F374,0x1F375,0x1F376);
-
-const devil = document.querySelector("#devil");
-devil.innerText = String.fromCodePoint(0x1F525,0x1F526,0x1F527,0x1F528,0x1F529,0x1F530);
-
-const clock = document.querySelector("#clock");
-clock.innerText = String.fromCodePoint(0x1F55B,0x1F55C,0x1F55D,0x1F55F,0x1F55A,0x1F552);
-
-const moon = document.querySelector("#moon");
-moon.innerText = String.fromCodePoint(0x1F319,0x1F318,0x1F317,0x1F316,0x1F312,0x1F314);
-
-const example = document.querySelector("#example");
-example.innerText = String.fromCodePoint(0x1F609);
-
-const example2 = document.querySelector("#example2");
-example2.innerText = String.fromCodePoint(0x1F610);
-
-const example3 = document.querySelector("#example3");
-example3.innerText = String.fromCodePoint(0x1F501);
-
+$container.innerHTML = container.join('')
 
